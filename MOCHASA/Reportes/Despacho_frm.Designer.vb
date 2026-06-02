042b5d421cc9860c865d14bb99016a53c8d9c082
Ingenieria1AQ <ingenieria1@ingematic.net>
2026-06-02 17:13:29 -0500

    feat: Orden de despacho con formato

--- MOCHASA/Reportes/Despacho_frm.Designer.vb
+++ MOCHASA/Reportes/Despacho_frm.Designer.vb
@@ -22,8 +22,11 @@ Partial Class Despacho_frm
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.crv = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -31,20 +34,23 @@ Partial Class Despacho_frm
         Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Controls.Add(Me.TabPage1)
         Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(781, 596)
+        Me.TabControl1.Size = New System.Drawing.Size(521, 387)
         Me.TabControl1.TabIndex = 1
         '
         'TabPage1
         '
         Me.TabPage1.Controls.Add(Me.crv)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(773, 563)
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TabPage1.Size = New System.Drawing.Size(513, 361)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "TICKET DE SALIDA"
         Me.TabPage1.UseVisualStyleBackColor = True
@@ -58,25 +64,49 @@ Partial Class Despacho_frm
         Me.crv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.crv.Cursor = System.Windows.Forms.Cursors.Default
         Me.crv.Location = New System.Drawing.Point(0, 0)
-        Me.crv.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.crv.Name = "crv"
         Me.crv.ShowGroupTreeButton = False
-        Me.crv.Size = New System.Drawing.Size(777, 567)
+        Me.crv.Size = New System.Drawing.Size(519, 369)
         Me.crv.TabIndex = 1
         Me.crv.ToolPanelWidth = 300
         '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.CrystalReportViewer1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(513, 361)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TICKET FORMATO"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = -1
+        Me.CrystalReportViewer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(-3, -4)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.ShowGroupTreeButton = False
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(519, 369)
+        Me.CrystalReportViewer1.TabIndex = 2
+        '
         'Despacho_frm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(780, 596)
+        Me.ClientSize = New System.Drawing.Size(520, 387)
         Me.Controls.Add(Me.TabControl1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Despacho_frm"
         Me.Text = "Egreso de Producto"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -84,4 +114,6 @@ Partial Class Despacho_frm
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents crv As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
 End Class
